e9ac7a6b4a56f182ccc5fa48423412ac658318f1
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2025-08-20 13:24:12 +0530

    Update ChangeCommitEmail.sh

--- ChangeCommitEmail.sh
+++ ChangeCommitEmail.sh
@@ -10,3 +10,17 @@ git filter-branch --env-filter '
          export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
      fi
      ' --tag-name-filter cat -- --branches --tags
+
+git filter-branch --env-filter '
+     CORRECT_EMAIL="rohitroshanjena2017@gmail.com"
+     if [ "$GIT_COMMITTER_EMAIL" != "$CORRECT_EMAIL" ]
+     then
+         export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+     fi
+     if [ "$GIT_AUTHOR_EMAIL" != "$CORRECT_EMAIL" ]
+     then
+         export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+     fi
+     ' --tag-name-filter cat -- --branches --tags
+
+
